30f2fb453eb97f57b30b0b5d550d365845acc6f6
Aljory <algori06ksa06@gmail.com>
2024-10-19 19:43:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter first number:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter operator (+, -, *, /):");
+        string op = Console.ReadLine();
+
+        Console.WriteLine("Enter second number:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = 0;
+
+        switch (op)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Invalid operator.");
+                break;
+        }
+
+        Console.WriteLine("Result: " + result);
+    }
+}
